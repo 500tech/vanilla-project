f4dbeebcedb3c38cd6e67fc45c73585f01133bc7
Alon Niv <oakfang@gmail.com>
2019-12-23 17:09:50 +0200

    initial

--- src/index.js
+++ src/index.js
@@ -1,12 +1,155 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import "normalize.css";
+import "./layout.css";
+import "./todos.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+/*
+interface Todo {
+  id: number;
+  title: string;
+  completed: bool;
+}
+*/
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+let _id = 0;
+const getId = () => _id++;
+const createTodo = (title, completed = false) => ({
+  title,
+  completed,
+  id: getId()
+});
+const noop = () => null;
+
+const TODOS = [
+  createTodo("Have morning coffee", true),
+  createTodo("Eat lunch", true),
+  createTodo("Make awesome things happen")
+];
+
+class TodoItem extends Component {
+  render() {
+    const { todo, onToggle = noop } = this.props;
+    return (
+      <li
+        tabIndex={0}
+        className={todo.completed ? "done" : ""}
+        onClick={() => {
+          onToggle(todo.id);
+        }}
+      >
+        {todo.title}
+      </li>
+    );
+  }
+}
+
+function TodoList({ todos, onToggle }) {
+  return (
+    <ul>
+      {todos.map((todo, index) => (
+        <TodoItem key={index} todo={todo} onToggle={onToggle} />
+      ))}
+    </ul>
+  );
+}
+
+class App extends Component {
+  state = {
+    todos: TODOS
+  };
+
+  toggleTodo = todoId => {
+    const { todos } = this.state;
+    const todosAfterChange = todos.map(todo => {
+      if (todo.id === todoId) {
+        return {
+          ...todo,
+          completed: !todo.completed
+        };
+      }
+      return todo;
+    });
+    this.setState({
+      todos: todosAfterChange
+    });
+  };
+
+  render() {
+    const { todos } = this.state;
+    return (
+      <>
+        <Header>ExCo.</Header>
+        <MainSection heading="My Todos List">
+          <TodoAdder />
+          <TodoList todos={todos} onToggle={this.toggleTodo} />
+          <PageControls />
+        </MainSection>
+        <Footer copyrightExpiary={2058} name="Example Corporation" />
+      </>
+    );
+  }
+}
+
+function Header({ children }) {
+  return (
+    <header>
+      <h1>{children}</h1>
+    </header>
+  );
+}
+
+function Footer({ copyrightExpiary, name }) {
+  const currentDate = new Date();
+  const year = currentDate.getUTCFullYear();
+  const showFooter = year <= copyrightExpiary;
+  if (!showFooter) {
+    return null;
+  }
+  return (
+    <footer>
+      <small>
+        &copy; Copyright {copyrightExpiary}, {name}
+      </small>
+    </footer>
+  );
+}
+
+function MainSection({ children, heading }) {
+  return (
+    <main id="todos-section">
+      <h2>{heading}</h2>
+      {children}
+    </main>
+  );
+}
+
+function PageControls() {
+  return (
+    <section className="controls">
+      <button id="mark-done">Mark all as done</button>
+      <button id="delete-done">Delete all done</button>
+    </section>
+  );
+}
+
+function TodoAdder() {
+  return (
+    <section>
+      <form id="todo-adder">
+        <input
+          type="text"
+          name="todo"
+          placeholder="Write up your todos"
+          required
+        />
+        <button>Add</button>
+      </form>
+    </section>
+  );
+}
+
+ReactDOM.render(
+  <App subheaderColor="blue" showSubheader />,
+  document.getElementById("root")
+);
