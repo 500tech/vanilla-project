8be0950a33aef9f2aaa3b56a15a4767342011b71
Alon Niv <oakfang@gmail.com>
2020-01-12 14:44:48 +0200

    remove legacy

--- src/ui/orgs/Todos/cells/TodoAdder.js
+++ src/ui/orgs/Todos/cells/TodoAdder.js
@@ -1,6 +1,4 @@
 import React, {
-  Component,
-  createRef,
   useState,
   useEffect,
   useCallback
@@ -54,73 +52,4 @@ export function TodoAdder({ onAddTodo = noop }) {
       </form>
     </section>
   );
-}
-
-export class TodoAdderLegacy extends Component {
-  state = {
-    text: ""
-  };
-
-  input = createRef();
-
-  componentDidMount() {
-    this.input.current.focus();
-  }
-
-  componentDidUpdate(_prevProps, prevState) {
-    if (prevState.text !== this.state.text) {
-      clearTimeout(this.autoSubmitTimer);
-      this.autoSubmitTimer = setTimeout(this.submit, 3000);
-    }
-  }
-
-  componentWillUnmount() {
-    clearTimeout(this.autoSubmitTimer);
-  }
-
-  setText(text) {
-    if (text.match(/clear/i)) {
-      return this.setText("");
-    }
-    this.setState({ text });
-  }
-
-  updateText = e => {
-    const text = e.target.value;
-    this.setText(text);
-  };
-
-  isValid = () => {
-    return this.state.text.length > 0;
-  };
-
-  submit = e => {
-    e && e.preventDefault();
-    if (this.isValid()) {
-      const { text } = this.state;
-      const { onAddTodo = noop } = this.props;
-      onAddTodo(text);
-      this.setText("");
-    }
-  };
-
-  render() {
-    const { text } = this.state;
-    return (
-      <section>
-        <form onSubmit={this.submit}>
-          <Input
-            ref={this.input}
-            type="text"
-            name="todo"
-            placeholder="Write up your todos"
-            required
-            onChange={this.updateText}
-            value={text}
-          />
-          <Button disabled={!this.isValid()}>Add</Button>
-        </form>
-      </section>
-    );
-  }
 }
